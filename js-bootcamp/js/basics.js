66abf601dc2729cdab4bb2bdd64eb1f358b773b4
Simun Strukan <simun.strukan@gmail.com>
2017-02-28 19:32:28 +0100

    added some exercises

--- js-bootcamp/js/basics.js
+++ js-bootcamp/js/basics.js
@@ -1,13 +1,87 @@
+var x = 5
+var y = 6
+var z = x + y
+
+var password = 'js-developer'
+
 var carsArray = [
 	"Opel",
 	"Renault",
 	"Citoren",
 	"Porcshe"
-
 ]
 
-var password = 'js-developer'
+var personObject = {
+	firstName: "Simun",
+	lastName: "Strukan",
+	position: "JavaScript developer",
+	email: "simun.strukan@gmail.com"
+}
 
+//Writting to console
+// console.log(z)
+// console.log(carsArray)
+
+//defining functions 
+function myFirstFunction(name) {
+
+	//setting some var in function
+
+	var msgContent = "Hello there," + name 
+
+	// console.log(msgContent)
+}
+
+//calling function 
+myFirstFunction("Simun")
+
+// alternative function definition
+var mySecondFunction = function(car){
+	var msgContent = "I drive " + car
+	return msgContent
+}
+
+// calling and loging function in same line and passing array element as function param
+// console.log(mySecondFunction(carsArray[1]))
+
+// Loops 
+for (var i = 0; i < carsArray.length; i++) {
+	// console.log(carsArray[i])
+}
+
+for (element in personObject) {
+	// console.log(personObject[element])
+}
+
+//callbacks 
+
+// function parentFunction(callback) {
+// 	//lets set some variable
+// 	var someString = "Hello there"
+
+// 	callback(someString) // whatever parameters we define here will ber passed to our callbackFunction
+// }
+
+// function callbackFunction(someString) {
+// 	//display alert
+// 	alert(someString)
+// }
+
+//parentFunction(callbackFunction)
+
+//alternate callbacks the jquery way
+
+// var myFunction = function(callback) {
+// 	var msgContent = "Hello world!"
+// 	callback(msgContent)
+// }
+
+// myFunction(function(msgContent){
+// 	alert(msgContent)
+// })
+
+
+// Using jquery
 $(document).ready(function(){
 	console.log("ready")
 
@@ -39,7 +113,7 @@ $(document).ready(function(){
 		if (passwordValid && emailValid) {
 			$('.output').text('You are cool')
 			$('#input-email').val('')
-			$('#input-password').val(')
+			$('#input-password').val('')
 		}
 
 	})
@@ -64,4 +138,33 @@ $(document).ready(function(){
 		$('#input-element').val('')
 	})
 })
+
+var comments = []
+
+//Small comments box
+$(document).ready(function(){
+	
+	var showComments = function() {
+		$('.comments').empty()
+		for (comment in comments) {
+			$('.comments').append('<p>' + comments[comment].username + '</p>')
+			$('.comments').append('<p>' + comments[comment].msg + '</p>')
+		}
+	}
+
+	
+	$('#comment-btn').click(function(){
+		console.log("comment")
+		var msg = $('#input-comment').val()
+		var username = $('#input-username').val()
+
+		var comment = {username: username, msg: msg}
+
+		comments.push(comment)
+		showComments()
+	})
+
+
+})
+
 
---
+++ js-bootcamp/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	// 1. Add login form
+	// 2. Input array element form
+	// 3. small comments app 
+
+})
